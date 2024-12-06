65290d50586ed13af9873457ffc504123d1e0c9f
Ken <zengkenwork@gmail.com>
2024-12-06 14:38:11 +0800

    add tryparse.html and  modify globalFunctions.js

--- JS/globalFunctions.js
+++ JS/globalFunctions.js
@@ -1,14 +1,44 @@
-function tryParseFloat(str) {
-  let result = {
-    isValid: false,
-    value: 0,
-  };
+let numberHelper = {
+  isInt: function (num) {
+    if (num instanceof Number) {
+      return num !== Nan && Number.isInteger(num.valueof());
+    }
+    return Number.isInteger(num);
+  },
+};
+let stringHelper = {
+  tryParseFloat: function (str) {
+    let result = {
+      isValid: false,
+      value: 0,
+    };
 
-  let num = Number(str);
+    let num = Number(str);
 
-  if (isNaN(str) == false) {
+    if (isNaN(str) == false) {
+      result.isValid = true;
+      result.value = num;
+    }
+    return result;
+  },
+  tryParseInt: function (str) {
+    let result = {
+      isValid: false,
+      value: 0,
+      error: "",
+    };
+
+    let floatResult = this.tryParseFloat(str);
+    if (!floatResult.isValid) return result;
+
+    if (!numberHelper.isInt(floatResult.value)) {
+      result.error = "無法順利轉為整數";
+      return result;
+    }
+
     result.isValid = true;
-    result.value = num;
-  }
-  return result;
-}
+    result.value = floatResult.value;
+
+    return result;
+  },
+};
